2336b9dbd184ee560a0b73071f0f9a24648a04e7
Lisandro Moreno <lisandromoreno28@gmail.com>
2024-05-08 10:54:28 -0300

    Se crean las carpetas principales del proyecto y configuracion de las rutas

---
+++ src/routes/RoutesViews.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+/* Todas las rutas del proyecto - Path / element  */
+
+const RoutesViews = () => {
+  return (
+    <>
+      <Routes>
+        <Route />
+      </Routes>
+    </>
+  );
+};
+
+export default RoutesViews;
