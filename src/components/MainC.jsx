15a776e7474a54e8ff012a007c54bcace751c906
Jose Martin Fe <martin.fesito@gmail.com>
2024-05-17 22:15:43 -0300

    Se realiza correcciones de Tags.

--- src/components/MainC.jsx
+++ src/components/MainC.jsx
@@ -17,39 +17,20 @@ const MainC = () => {
       </Pagination.Item>
     );
   }
+
   return (
-    <div className="container-fluid mt-30">
-      <div className="row px-xl-5 ">
-        <div className="col-lg-8 mb-30">
-          <Carousel fade className="h-100 ">
-            <Carousel.Item>
-              <div className="productH2">
-                <Image
-                  urlImagen="https://cdn.euroinnova.edu.es/img/subidasEditor/curso-1612925686.webp"
-                  alternativo="Descripción de la primera imagen"
-                  ancho={"100%"} // Ancho de la imagen, puedes ajustarlo según tus necesidades
-                />
-              </div>
-              <Carousel.Caption>
-                <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
-                  <div className="p-3 captionMW">
-                    <h1 className="display-4 text-white mb-3 animate__animated animate__fadeInDown">
-                      Nuestros Profesionales
-                    </h1>
-                    <p className="mx-md-5 px-5">
-                      Nuestros profesionales están comprometidos con la salud y
-                      el bienestar de las mascotas. ¡Confía en ellos para el
-                      cuidado de tus animales!
-                    </p>
-                    <a
-                      className="btn btn-outline-light py-2 px-4 mt-3 animate__animated animate__fadeInDown"
-                      href="/profesionales"
-                    >
-                      Ver más
-                    </a>
-                  </div>
-
-
+    <>
+      <div className="container-fluid mt-30">
+        <div className="row px-xl-5 ">
+          <div className="col-lg-8 mb-30">
+            <Carousel fade className="h-100 ">
+              <Carousel.Item>
+                <div className="productH2">
+                  <Image
+                    urlImagen="https://cdn.euroinnova.edu.es/img/subidasEditor/curso-1612925686.webp"
+                    alternativo="Descripción de la primera imagen"
+                    ancho={"100%"} // Ancho de la imagen, puedes ajustarlo según tus necesidades
+                  />
                 </div>
                 <Carousel.Caption>
                   <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
@@ -64,7 +45,8 @@ const MainC = () => {
                       </p>
                       <a
                         className="btn btn-outline-light py-2 px-4 mt-3 animate__animated animate__fadeInDown"
-                        href="/profesionales">
+                        href="/profesionales"
+                      >
                         Ver más
                       </a>
                     </div>
@@ -91,7 +73,8 @@ const MainC = () => {
                       </p>
                       <a
                         className="btn btn-outline-light py-2 px-4 mt-3 animate__animated animate__fadeInDown"
-                        href="/detallePlanes">
+                        href="/detallePlanes"
+                      >
                         Ver más
                       </a>
                     </div>
@@ -118,7 +101,8 @@ const MainC = () => {
                       </p>
                       <a
                         className="btn btn-outline-light py-2 px-4 mt-3 animate__animated animate__fadeInDown"
-                        href="#">
+                        href="#"
+                      >
                         Ver más
                       </a>
                     </div>
@@ -138,7 +122,7 @@ const MainC = () => {
               <div className="offer-text">
                 <h6 className="text-white text-uppercase">Descuento 20%</h6>
                 <h3 className="text-white mb-3">Oferta especial</h3>
-                <a href="" className="btn btn-outline-light py-2 px-4 mt-3">
+                <a href="#" className="btn btn-outline-light py-2 px-4 mt-3">
                   Ver más
                 </a>
               </div>
@@ -147,20 +131,19 @@ const MainC = () => {
               <Image
                 className="img-fluid"
                 urlImagen="https://cdn.ready-market.com.tw/78d8bdd7/Templates/pic/Dog-Toy-1.jpg"
-                alternativo="Descripción de la imagen derecha 1"
+                alternativo="Descripción de la imagen derecha 2"
                 ancho={"100%"} // Ancho de la imagen, puedes ajustarlo según tus necesidades
               />
               <div className="offer-text">
                 <h6 className="text-white text-uppercase">Descuento 20%</h6>
                 <h3 className="text-white mb-3">Oferta especial</h3>
-                <a href="" className="btn btn-outline-light py-2 px-4 mt-3">
+                <a href="#" className="btn btn-outline-light py-2 px-4 mt-3">
                   Ver más
                 </a>
               </div>
             </div>
           </div>
         </div>
-
       </div>
 
       <div className="container-fluid">
@@ -224,57 +207,56 @@ const MainC = () => {
                   alternativo="logo de Royal Canin"
                   ancho="25%"
                 />
-
               </div>
-              <div className="col-lg-4 col-md-6 mb-4">
-                <div className="card-size">
-                  <Image
-                    className="card-img-top"
-                    urlImagen="https://1000logos.net/wp-content/uploads/2020/09/Pedigree_logo_PNG7.png"
-                    alternativo="logo de Pedigree"
-                    ancho="25%"
-                  />
-                </div>
+            </div>
+            <div className="col-lg-4 col-md-6 mb-4">
+              <div className="card-size">
+                <Image
+                  className="card-img-top"
+                  urlImagen="https://1000logos.net/wp-content/uploads/2020/09/Pedigree_logo_PNG7.png"
+                  alternativo="logo de Pedigree"
+                  ancho="25%"
+                />
               </div>
-              <div className="col-lg-4 col-md-6 mb-4 mt-4">
-                <div className="card-size">
-                  <Image
-                    className="card-img-top"
-                    urlImagen="https://1000logos.net/wp-content/uploads/2020/09/Purina_logo_PNG3.png"
-                    alternativo="logo de Purina"
-                    ancho="25%"
-                  />
-                </div>
+            </div>
+            <div className="col-lg-4 col-md-6 mb-4 mt-4">
+              <div className="card-size">
+                <Image
+                  className="card-img-top"
+                  urlImagen="https://1000logos.net/wp-content/uploads/2020/09/Purina_logo_PNG3.png"
+                  alternativo="logo de Purina"
+                  ancho="25%"
+                />
               </div>
-              <div className="col-lg-4 col-md-6 mb-3">
-                <div className="card-size">
-                  <Image
-                    className="card-img-top"
-                    urlImagen="https://1000logos.net/wp-content/uploads/2023/10/Eukanuba_logo_PNG5.png"
-                    alternativo="logo de Eukanuba"
-                    ancho="25%"
-                  />
-                </div>
+            </div>
+            <div className="col-lg-4 col-md-6 mb-3">
+              <div className="card-size">
+                <Image
+                  className="card-img-top"
+                  urlImagen="https://1000logos.net/wp-content/uploads/2023/10/Eukanuba_logo_PNG5.png"
+                  alternativo="logo de Eukanuba"
+                  ancho="25%"
+                />
               </div>
-              <div className="col-lg-4 col-md-6 mb-3">
-                <div className="card-size">
-                  <Image
-                    className="card-img-top"
-                    urlImagen="https://1000logos.net/wp-content/uploads/2021/03/Whiskas_logo_PNG7.png"
-                    alternativo="logo de Whiskas"
-                    ancho="25%"
-                  />
-                </div>
+            </div>
+            <div className="col-lg-4 col-md-6 mb-3">
+              <div className="card-size">
+                <Image
+                  className="card-img-top"
+                  urlImagen="https://1000logos.net/wp-content/uploads/2021/03/Whiskas_logo_PNG7.png"
+                  alternativo="logo de Whiskas"
+                  ancho="25%"
+                />
               </div>
-              <div className="col-lg-4 col-md-6 mb-3">
-                <div className="card-size">
-                  <Image
-                    className="card-img-top"
-                    urlImagen="https://1000logos.net/wp-content/uploads/2020/09/Friskies_logo_PNG7.png"
-                    alternativo="logo de Friskies"
-                    ancho="25%"
-                  />
-                </div>
+            </div>
+            <div className="col-lg-4 col-md-6 mb-3">
+              <div className="card-size">
+                <Image
+                  className="card-img-top"
+                  urlImagen="https://1000logos.net/wp-content/uploads/2020/09/Friskies_logo_PNG7.png"
+                  alternativo="logo de Friskies"
+                  ancho="25%"
+                />
               </div>
             </div>
           </div>
