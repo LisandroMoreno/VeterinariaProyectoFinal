237d0ba5bf7b785ddacad451d03007519154f916
Jose Martin Fe <martin.fesito@gmail.com>
2024-05-27 09:50:52 -0300

    Se realiza modificacion de css y vista de detalle de producto, Creacion de input cantidad con su modificacion de precio

--- src/pages/DetalleProducto.jsx
+++ src/pages/DetalleProducto.jsx
@@ -1,4 +1,11 @@
-import { Col, Container, Row, Button } from "react-bootstrap";
+import {
+  Col,
+  Container,
+  Row,
+  Button,
+  InputGroup,
+  FormControl,
+} from "react-bootstrap";
 import { titlePage } from "../helpers/titlePages";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -11,11 +18,14 @@ titlePage("Detalle de Producto");
 const DetalleProducto = () => {
   const params = useParams();
   const [product, setProduct] = useState({});
+  const [cantidad, setCantidad] = useState(1);
+  const [precioTotal, setPrecioTotal] = useState(0);
 
   const obtenerProducto = async () => {
     try {
       const response = await clienteAxios.get(`/productos/${params.id}`);
       setProduct(response.data.product);
+      setPrecioTotal(response.data.product.precio);
     } catch (error) {
       console.error("Error al obtener el producto:", error);
     }
@@ -24,6 +34,12 @@ const DetalleProducto = () => {
   useEffect(() => {
     obtenerProducto();
   }, [params.id]);
+
+  const actualizarCantidad = (e) => {
+    const nuevaCantidad = parseInt(e.target.value, 10);
+    setCantidad(nuevaCantidad);
+    setPrecioTotal(nuevaCantidad * product.precio);
+  };
 
   const agregarFavoritos = () => {
     // Lógica para agregar a favoritos
@@ -47,28 +63,38 @@ const DetalleProducto = () => {
           </div>
         </Col>
         <Col xs="12" md="6">
+          <div className="text-end mt-4">
+            <a href="">
+              <i className="fa-solid fa-heart fa-2x icono-favorito"></i>
+            </a>
+          </div>
+
           <div className="product-details">
             <h2>{product.titulo}</h2>
-            <p>{product.descripcion}</p>
-            <p>${product.precio}</p>
+            <p className="mt-3">
+              <strong>Precio: $</strong> {precioTotal.toFixed(2)}
+            </p>
+
             <div className="buttons-container">
-              <Button
-                variant="primary"
-                onClick={agregarFavoritos}
-                className="mt-2"
-              >
-                Agregar a favoritos
-              </Button>
-              <Button
-                variant="primary"
-                onClick={agregarCarrito}
-                className="mt-2"
-              >
-                Agregar al Carrito
+              <InputGroup className="w-50 mb-3">
+                <InputGroup.Text>Cantidad</InputGroup.Text>
+                <FormControl
+                  type="number"
+                  value={cantidad}
+                  min="1"
+                  onChange={actualizarCantidad}
+                />
+              </InputGroup>
+              <Button className="btn-primary mb-3">Comprar</Button>
+              <Button onClick={agregarCarrito} className="btn-primary mb-3">
+                <i className="fa-solid fa-cart-shopping"></i>
               </Button>
             </div>
-            <div>
-              <Button className="mt-2 producto">Comprar</Button>
+            <div className="mt-3">
+              <p>
+                <strong>Descripcion:</strong>
+              </p>
+              <p>{product.descripcion}</p>
             </div>
           </div>
         </Col>
