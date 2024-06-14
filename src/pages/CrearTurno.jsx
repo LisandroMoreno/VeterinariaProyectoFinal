5586f9ef8ffbfc1822f6f57999a605cd29546299
Jose Martin Fe <martin.fesito@gmail.com>
2024-06-13 23:26:33 -0300

    Se realiza modificaciones del componente CrearTurno,agregando libreria moment, filtrar horarios disponibles segun veterinarios, funcionalidades al elegir el veterinario filtrar los turnos disponibles

--- src/pages/CrearTurno.jsx
+++ src/pages/CrearTurno.jsx
@@ -2,109 +2,88 @@ import React, { useState, useEffect } from "react";
 import DatePicker, { registerLocale } from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import es from "date-fns/locale/es";
+import moment from "moment-timezone";
 import clienteAxios, { config } from "../helpers/clienteAxios";
 import "../css/CrearTurno.css";
-import { titlePage } from "../helpers/titlePages";
 
 registerLocale("es", es);
 
-titlePage("Reserva de turnos");
-
-const veterinarios = [
-  { nombre: "Seleccione el veterinario", foto: "" },
-  { nombre: "Dra. Maria Rodriguez", foto: "../src/img/Profesional1.jpg" },
-  { nombre: "Dr. Juan Perez Torasso", foto: "../src/img/Profesional2.jpg" },
-];
-
-const detallesCita = [
-  "Seleccione la consulta",
-  "Consulta de rutina",
-  "Vacunación",
-  "Cirugía menor",
-];
-
-const generarOpcionesTiempo = () => {
-  const opciones = [];
-  const inicio = 9 * 60; // 9:00 AM en minutos
-  const fin = 17 * 60; // 5:00 PM en minutos
-  for (let i = inicio; i < fin; i += 20) {
-    const horas = Math.floor(i / 60);
-    const minutos = i % 60;
-    const horaFormateada = `${horas.toString().padStart(2, "0")}:${minutos
-      .toString()
-      .padStart(2, "0")}`;
-    opciones.push(horaFormateada);
-  }
-  return opciones;
-};
-
-const opcionesTiempo = generarOpcionesTiempo();
-
 const CrearTurno = () => {
   const [formData, setFormData] = useState({
-    detalleCita: "Seleccione la consulta",
-    veterinario: "Seleccione el veterinario",
+    detalleCita: "",
+    veterinario: "",
     mascota: "",
     fecha: new Date(),
-    hora: opcionesTiempo[0],
+    hora: "",
   });
 
+  const [veterinarios, setVeterinarios] = useState([]);
   const [fotoVet, setFotoVet] = useState("");
-  const [turnosSeleccionados, setTurnosSeleccionados] = useState([]);
-
-  useEffect(() => {
-    const fetchTurnos = async () => {
-      try {
-        const res = await clienteAxios.get("/turnos", config);
-        const turnos = res.data.map((turno) => turno.reservas);
-        const flattenTurnos = [].concat.apply([], turnos);
-        setTurnosSeleccionados(flattenTurnos);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchTurnos();
-  }, []);
+  const [opcionesTiempoDisponibles, setOpcionesTiempoDisponibles] = useState(
+    []
+  );
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData({
-      ...formData,
+    setFormData((prevFormData) => ({
+      ...prevFormData,
       [name]: value,
-    });
+    }));
 
     if (name === "veterinario") {
       const selectedVet = veterinarios.find((vet) => vet.nombre === value);
-      setFotoVet(selectedVet ? selectedVet.foto : "");
+      setFotoVet(selectedVet?.foto || "");
+      actualizarOpcionesDisponibles(formData.fecha, value);
     }
   };
 
   const handleDateChange = (date) => {
-    setFormData({
-      ...formData,
+    setFormData((prevFormData) => ({
+      ...prevFormData,
       fecha: date,
-    });
+    }));
+    actualizarOpcionesDisponibles(date, formData.veterinario);
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    const fechaFormateada = moment(formData.fecha)
+      .tz("America/Argentina/Buenos_Aires")
+      .format("YYYY-MM-DD");
+
+    if (!formData.hora) {
+      alert("Por favor, seleccione una hora válida.");
+      return;
+    }
+
     try {
-      const res = await clienteAxios.post("/turnos", formData, config);
-      setFormData({
-        detalleCita: "Seleccione la consulta",
-        veterinario: "Seleccione el veterinario",
-        mascota: "",
-        fecha: new Date(),
-        hora: opcionesTiempo[0],
-      });
-      setFotoVet("");
-      setTurnosSeleccionados([...turnosSeleccionados, formData]);
+      const response = await clienteAxios.post(
+        "/turnos",
+        {
+          ...formData,
+          fecha: fechaFormateada,
+        },
+        config
+      );
       alert("Turno creado exitosamente");
+      resetForm();
     } catch (err) {
       console.error("Error al crear el turno:", err);
-      alert("Hubo un error al crear el turno");
+      alert(err.response.data.message); // Muestra el mensaje de error del backend
     }
+  };
+
+  const resetForm = () => {
+    setFormData({
+      detalleCita: "",
+      veterinario: "",
+      mascota: "",
+      fecha: new Date(),
+      hora: "",
+    });
+    setFotoVet("");
+    setOpcionesTiempoDisponibles([]);
   };
 
   const isWeekday = (date) => {
@@ -112,21 +91,43 @@ const CrearTurno = () => {
     return day !== 0 && day !== 6; // 0 es domingo, 6 es sábado
   };
 
-  const obtenerTurnosVet = (veterinario, fecha) => {
-    return turnosSeleccionados
-      .filter(
-        (reserva) =>
-          reserva.veterinario === veterinario &&
-          new Date(reserva.fecha).toDateString() ===
-            new Date(fecha).toDateString()
-      )
-      .map((reserva) => reserva.hora);
+  const actualizarOpcionesDisponibles = async (fecha, veterinario) => {
+    if (!veterinario || !fecha) return;
+
+    const fechaFormateada = moment(fecha)
+      .tz("America/Argentina/Buenos_Aires")
+      .format("YYYY-MM-DD");
+
+    try {
+      const response = await clienteAxios.post(
+        "/turnos/disponibles",
+        { fecha: fechaFormateada, veterinario },
+        config
+      );
+      setOpcionesTiempoDisponibles(response.data);
+    } catch (err) {
+      console.error("Error al obtener los turnos disponibles:", err);
+    }
   };
 
-  const opcionesTiempoDisponibles = opcionesTiempo.filter(
-    (hora) =>
-      !obtenerTurnosVet(formData.veterinario, formData.fecha).includes(hora)
-  );
+  useEffect(() => {
+    const fetchVeterinarios = async () => {
+      try {
+        const res = await clienteAxios.get(
+          "/profesionales/profesionales",
+          config
+        );
+        setVeterinarios(res.data);
+        if (res.data.length > 0) {
+          actualizarOpcionesDisponibles(formData.fecha, res.data[0].nombre);
+        }
+      } catch (error) {
+        console.error("Error al obtener veterinarios:", error);
+      }
+    };
+
+    fetchVeterinarios();
+  }, []);
 
   return (
     <div className="crear-turno-container">
@@ -147,11 +148,16 @@ const CrearTurno = () => {
               value={formData.detalleCita}
               onChange={handleChange}
             >
-              {detallesCita.map((detalle, index) => (
-                <option key={index} value={detalle}>
-                  {detalle}
-                </option>
-              ))}
+              <option value="" disabled>
+                Seleccione la consulta
+              </option>
+              {["Consulta de rutina", "Vacunación", "Cirugía menor"].map(
+                (detalle, index) => (
+                  <option key={index} value={detalle}>
+                    {detalle}
+                  </option>
+                )
+              )}
             </select>
           </div>
 
@@ -162,6 +168,9 @@ const CrearTurno = () => {
               value={formData.veterinario}
               onChange={handleChange}
             >
+              <option value="" disabled>
+                Seleccione el veterinario/a
+              </option>
               {veterinarios.map((vet) => (
                 <option key={vet.nombre} value={vet.nombre}>
                   {vet.nombre}
@@ -169,7 +178,6 @@ const CrearTurno = () => {
               ))}
             </select>
           </div>
-
           <div className="form-group">
             <label htmlFor="mascota">Mascota</label>
             <input
@@ -196,6 +204,9 @@ const CrearTurno = () => {
           <div className="form-group">
             <label htmlFor="hora">Hora</label>
             <select name="hora" value={formData.hora} onChange={handleChange}>
+              <option value="" disabled>
+                Seleccione la hora
+              </option>
               {opcionesTiempoDisponibles.map((hora, index) => (
                 <option key={index} value={hora}>
                   {hora}
