90f38ff11b1ba115b43e179faefa2ee0960dfdce
Jose Martin Fe <149723252+Jose-Martin-Fe@users.noreply.github.com>
2024-07-01 20:42:57 -0300

    Se realiza corrección de ruta protegida para redirigir según el rol o token existente

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,22 +1,35 @@
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
 const PrivateRoute = ({ children, role }) => {
   const token = JSON.parse(sessionStorage.getItem("token")) || "";
   const roleUser = JSON.parse(sessionStorage.getItem("role")) || "";
-
   const navigate = useNavigate();
 
-  if (!token) {
-    navigate("/login");
-  } else {
-    if (role === roleUser) {
-      return children;
+  useEffect(() => {
+    if (!token) {
+      navigate("/login");
     } else {
-      if (roleUser === "admin") {
-        navigate("/home-adminLog");
-      } else {
-        navigate("/");
+      if (role !== roleUser) {
+        if (roleUser === "admin") {
+          navigate("/home-adminLog");
+        } else {
+          navigate("/");
+        }
       }
     }
+  }, [token, role, roleUser, navigate]);
+
+  if (!token) {
+    return null; 
   }
+
+  if (role === roleUser) {
+    return children;
+  }
+
+
+  return null;
 };
+
 export default PrivateRoute;
